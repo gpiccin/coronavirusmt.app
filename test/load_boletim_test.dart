f7146f4ee4ad71d9a661440d82ec6143aee6f8f0
Gustavo Piccin <gpiccin@gmail.com>
2020-04-15 01:49:17 -0400

    Carregamento do modelo do boletim.

--- test/load_boletim_test.dart
+++ test/load_boletim_test.dart
@@ -7,10 +7,28 @@ void main() {
 
     expect(boletim.referencia, "37");
     expect(boletim.data, DateTime.parse("2020-04-14"));
+    expect(boletim.link, "http://www.saude.mt.gov.br/arquivo/10932");
+
     expect(boletim.sragCasos, 537);
+    expect(boletim.sragNovos, 21);
+
     expect(boletim.covidCasos, 138);
+    expect(boletim.covidNovos, 4);
+
     expect(boletim.covidHospitalizados, 6);
+    expect(boletim.covidNovosHospitalizados, -2);
+    expect(boletim.covidPercentualDeHospitalizados, 4);
+
     expect(boletim.covidRecuperados, 31);
+    expect(boletim.covidNovosRecuperados, 14);
+    expect(boletim.covidPercentualDeRecuperados, 22);
+
     expect(boletim.covidObitos, 4);
+    expect(boletim.covidNovosObitos, 0);
+    expect(boletim.covidPercentualDeObitos, 3);
+
+    expect(boletim.covidIsolamento, 97);
+    expect(boletim.covidNovosEmIsolamento, -8);
+    expect(boletim.covidPercentualEmIsolamento, 70);
   });
 }
