9b904ceb9d9d7f005d05747763a8c6528e949972
wesley viana gomes <javagora@gmail.com>
2020-04-15 01:07:36 -0400

    correcao chatas de nome

--- lib/shared/repository/app_repository.dart
+++ lib/shared/repository/app_repository.dart
@@ -1,17 +1,25 @@
 import 'package:covidmt/model/boletim.dart';
+import 'package:covidmt/model/srars_model.dart';
 import 'package:covidmt/shared/repository/utils/constants.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
-class AppRepositoriy {
-  static Future<Boletim> getDadosBoletim() async {
+class AppRepository {
+  static Future<List<SrasModel>> getDadosBoletim() async {
     var client = Dio(BaseOptions(baseUrl: Constants.URL_KEYCOVIDMT));
 
     Response response = await client.post(Constants.KEY_COVIDMT, data: {
       "query":
           "query {\n  boletims(limit: 1, sort:\"data:desc\") {\n    data\n    srag_casos_total\n    covid_casos_total\n  }\nsummary: boletimsConnection {\n    aggregate {\n      count\n      avg {\n        srag_casos_novos\n        covid_casos_novos\n      }\n    }\n  }\n}"
     });
+    var itens = response.data;
 
-    print(response);
+    List<SrasModel> lista_boletim_sars = [];
+    for (int i = 0; i < itens.length; i++) {
+      SrasModel srars = SrasModel.fromJson(itens[i]);
+      lista_boletim_sars.add(srars);
+    }
+
+    return lista_boletim_sars;
   }
 }
