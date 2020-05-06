4872bceae88fcced91fe5b601cd327f43fbb7c44
Gustavo Piccin <gpiccin@gmail.com>
2020-05-05 22:05:22 -0400

    New style. New bottom bar.

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,9 +1,9 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:coronavirusmt/ui/covid/views/boletins.dart';
 import 'package:coronavirusmt/ui/home/views/situacao_atual.dart';
 import 'package:coronavirusmt/ui/home/widgets/header.dart';
 import 'package:coronavirusmt/ui/noticias/noticias.dart';
 import 'package:coronavirusmt/ui/shared/ui_style.dart';
-import 'package:coronavirusmt/ui/shared/ui_typography.dart';
 import 'package:flutter/material.dart';
 
 // Somewhere in your widgets...
@@ -16,55 +16,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   static List<Widget> _widgetOptions = <Widget>[
     SituacaoAtualView(),
     NoticiasView(),
     BoletinsView(),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  BottomNavigationBarItem _navigationBarItem(IconData icon, String text) {
-    return BottomNavigationBarItem(
-      icon: Icon(
-        icon,
-        size: 20,
-      ),
-      title: Padding(
-        padding: const EdgeInsets.only(top: 2.0),
-        child: Text(text, style: UITypography.button),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: UIStyle.appBackgroundColor,
-      body: Column(children: <Widget>[
-        Header(),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(
-                UIStyle.padding, 0, UIStyle.padding, 0),
-            child: _widgetOptions.elementAt(_selectedIndex),
+        backgroundColor: UIStyle.appBackgroundColor,
+        body: Column(children: <Widget>[
+          Header(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(
+                  UIStyle.padding, 0, UIStyle.padding, 0),
+              child: PageView(
+                controller: _pageController,
+                children: _widgetOptions,
+              ),
+            ),
           ),
-        )
-      ]),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          this._navigationBarItem(Icons.show_chart, "Situação atual"),
-          this._navigationBarItem(Icons.public, "Notícias"),
-          this._navigationBarItem(Icons.library_books, "Boletins"),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: UIStyle.casosColor,
-        onTap: _onItemTapped,
-      ),
-    );
+        ]),
+        bottomNavigationBar: BottomNavyBar(
+          backgroundColor: UIStyle.appBackgroundColor,
+
+          selectedIndex: _selectedIndex,
+          showElevation: true, // use this to remove appBar's elevation
+          onItemSelected: (index) => setState(() {
+            _selectedIndex = index;
+            _pageController.animateToPage(index,
+                duration: Duration(milliseconds: 300), curve: Curves.ease);
+          }),
+          items: [
+            BottomNavyBarItem(
+              icon: Icon(Icons.show_chart),
+              title: Text('Situação', textAlign: TextAlign.center),
+              activeColor: UIStyle.casosColor,
+            ),
+            BottomNavyBarItem(
+                icon: Icon(Icons.public),
+                title: Text('Notícias', textAlign: TextAlign.center),
+                activeColor: UIStyle.casosColor),
+            BottomNavyBarItem(
+                icon: Icon(Icons.library_books),
+                title: Text('Boletins', textAlign: TextAlign.center),
+                activeColor: UIStyle.casosColor),
+          ],
+        ));
   }
 }
